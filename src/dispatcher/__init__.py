af020325ac12b9984d07ec1f31ecd074b75e4286
Valentin Ambroise <valentin.ambroise@outlook.com>
2023-07-19 08:06:03 +0200

    Expose `Serializer` in the package namespace

--- src/dispatcher/__init__.py
+++ src/dispatcher/__init__.py
@@ -1,10 +1,11 @@
 from .ABC import AsyncDispatcher, context, Dispatcher, STOP_SIGNAL
-from .async_in_memory_dispatcher import AsyncInMemoryDispatcher
 from .async_amqp_dispatcher import AsyncAMQPDispatcher
+from .async_in_memory_dispatcher import AsyncInMemoryDispatcher
 from .async_redis_dispatcher import AsyncRedisDispatcher
-from .in_memory_dispatcher import InMemoryDispatcher
 from .context_var_wrapper import ContextVarWrapper
 from .event_handler import AsyncEventHandler, EventHandler
 from .exceptions import StopEvent, UnknownEvent
+from .in_memory_dispatcher import InMemoryDispatcher
 from .kombu_dispatcher import KombuDispatcher
+from .serializer import Serializer
 from .utils import RegisterEventMixin
