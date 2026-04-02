691618dc8bc93cd7c800c5e2d07c30e0cbc05212
Valentin Ambroise <valentin.ambroise@outlook.com>
2026-04-02 23:37:14 +0200

    Clean-up `utils.py`

--- src/dispatcher/utils.py
+++ src/dispatcher/utils.py
@@ -3,12 +3,6 @@ from __future__ import annotations
 from .ABC import AsyncDispatcher, Dispatcher
 
 
-try:
-    import kombu
-except ImportError:
-    kombu = None
-
-
 class RegisterEventMixin:
     def register_dispatcher_events(self, dispatcher: AsyncDispatcher | Dispatcher) -> None:
         """Register the methods starting by "dispatch_" as an event handler"""
